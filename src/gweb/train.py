36a080eb8fe9bdc31f9dbddbbc62bed4b5406a66
Johanne Birk Christensen <jbirkc@icloud.com>
2025-01-10 14:04:25 +0100

    second iteration train

--- src/gweb/train.py
+++ src/gweb/train.py
@@ -1,9 +1,14 @@
 import torch
 import torch_geometric.transforms as T
 from torch_geometric.loader import NeighborLoader
+import matplotlib.pyplot as plt
+import torch
+import typer
+from .data import datanavnhertak!
+from .model import GCN
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-dataset = AMLtoGraph('/path/to/AntiMoneyLaunderingDetectionWithGNN/data')
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+dataset = AMLtoGraph('/path/to/AntiMoneyLaunderingDetectionWithGNN/data') # write correct path. 
 data = dataset[0]
 epoch = 100
 
@@ -55,3 +60,111 @@ for i in range(epoch):
             acc += correct
         acc = acc/total
         print('accuracy:', acc)
+        
+        
+        
+if __name__ == "__main__":
+    typer.run(train)
+    
+    
+    
+def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+    """Train a model on MNIST."""
+    print("Training day and night")
+    print(f"{lr=}, {batch_size=}, {epochs=}")
+
+    model = GCN(in_channels=data.num_features, hidden_channels=16, out_channels=1, heads=8)
+    model = model.to(device)
+    criterion = torch.nn.BCELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
+
+    split = T.RandomNodeSplit(split='train_rest', num_val=0.1, num_test=0)
+    data = split(data)
+    
+  
+
+    statistics = {"train_loss": [], "train_accuracy": []}
+    for epoch in range(epochs):
+        model.train()
+        for i, (img, target) in enumerate(train_dataloader):
+            img, target = img.to(DEVICE), target.to(DEVICE)
+            optimizer.zero_grad()
+            y_pred = model(img)
+            loss = loss_fn(y_pred, target)
+            loss.backward()
+            optimizer.step()
+            statistics["train_loss"].append(loss.item())
+
+            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+            statistics["train_accuracy"].append(accuracy)
+
+            if i % 100 == 0:
+                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+
+    print("Training complete")
+    torch.save(model.state_dict(), "models/model.pth")
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    axs[0].plot(statistics["train_loss"])
+    axs[0].set_title("Train loss")
+    axs[1].plot(statistics["train_accuracy"])
+    axs[1].set_title("Train accuracy")
+    fig.savefig("reports/figures/training_statistics.png")
+
+
+if __name__ == "__main__":
+    typer.run(train)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10) -> None:
+    """Train a model on MNIST."""
+    print("Training day and night")
+    print(f"{lr=}, {batch_size=}, {epochs=}")
+
+    model = MyAwesomeModel().to(DEVICE)
+    train_set, _ = corrupt_mnist()
+
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    statistics = {"train_loss": [], "train_accuracy": []}
+    for epoch in range(epochs):
+        model.train()
+        for i, (img, target) in enumerate(train_dataloader):
+            img, target = img.to(DEVICE), target.to(DEVICE)
+            optimizer.zero_grad()
+            y_pred = model(img)
+            loss = loss_fn(y_pred, target)
+            loss.backward()
+            optimizer.step()
+            statistics["train_loss"].append(loss.item())
+
+            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+            statistics["train_accuracy"].append(accuracy)
+
+            if i % 100 == 0:
+                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+
+    print("Training complete")
+    torch.save(model.state_dict(), "models/model.pth")
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    axs[0].plot(statistics["train_loss"])
+    axs[0].set_title("Train loss")
+    axs[1].plot(statistics["train_accuracy"])
+    axs[1].set_title("Train accuracy")
+    fig.savefig("reports/figures/training_statistics.png")
+
+
+if __name__ == "__main__":
+    typer.run(train)
