698afe868a9aee8dd37eb5ddc567afe7cf50cd76
a-estad <ahestad@outlook.com>
2025-01-21 13:49:30 +0100

    cleanup

--- src/gweb/__init__.py
+++ src/gweb/__init__.py
@@ -3,4 +3,3 @@ _GWEB_ROOT = os.path.dirname(__file__)
 _SRC_ROOT = os.path.dirname(_GWEB_ROOT)
 _PROJECT_ROOT = os.path.dirname(_SRC_ROOT)
 _PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
-print(_PATH_DATA)
